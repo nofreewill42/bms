sampling weights calculation; tiny fix of model; data loading and training parameters

data_loaders/data_loader.py
@@ -20,7 +20,6 @@ class DS(Dataset):
 
         # Augment
         self.rotate_tfms = KA.RandomAffine(3., p=1., keepdim=True)
-        self.cutout_tfms = T.Compose(4*[KA.RandomErasing((0.003,0.003),p=1., keepdim=True)])
 
         # Fill with with indices before each epoch
         self.batches = []
@@ -100,22 +99,22 @@ class DS(Dataset):
     def get_new_sizes(self, w,h):#,W,H):
         rs = 0.08
         rs = random.uniform(1-rs, 1+rs)
-        rrm = 0.47
+        rrm = 0.57
         rrM = min(1/rrm, self.img_size/max(w*rs,h))  # max out at img_size
         rrm, rrM = np.log(rrm), np.log(rrM)
         rr = random.uniform(rrm, rrM)
         rr = np.exp(rr)
         return int(rr*w*rs), int(rr*h)
 
-    def erase_tfms(self, img_tensor, n=4, r=0.1):
+    def erase_tfms(self, img_tensor, n=4, r=0.04):
         _,h,w = img_tensor.shape
         max_cut_size = max(h,w)
         h_idxs = torch.arange(h).reshape(-1,1)
         w_idxs = torch.arange(w).reshape(1,-1)
         h_cut_start = torch.randint(h, size=(1,n))
         w_cut_start = torch.randint(w, size=(n,1))
-        h_cut_size = torch.randint(int(max_cut_size*r), size=(1,n))
-        w_cut_size = torch.randint(int(max_cut_size*r), size=(n,1))
+        h_cut_size = torch.randint(int(max_cut_size*r)+1, size=(1,n))
+        w_cut_size = torch.randint(int(max_cut_size*r)+1, size=(n,1))
         h_cuts = (h_cut_start<h_idxs)*(h_idxs<h_cut_start+h_cut_size)
         w_cuts = (w_cut_start<w_idxs)*(w_idxs<w_cut_start+w_cut_size)
         cuts = (h_cuts.float() @ w_cuts.float()).clamp(max=1.)*-1+1

model_architecture/pytorch_transformer/transformer.py
@@ -66,6 +66,8 @@ class TransformerEncoderLayer(Module):
         self.dropout1 = Dropout(dropout)
         self.dropout2 = Dropout(dropout)
 
+        self.activation = F.gelu
+
     def forward(self, src: Tensor, src_mask: Optional[Tensor] = None, src_key_padding_mask: Optional[Tensor] = None,
                 dropout_h: float = 0.) -> Tensor:
         src2 = self.self_attn(src, src, src, attn_mask=src_mask,
@@ -73,7 +75,7 @@ class TransformerEncoderLayer(Module):
                               dropout_h=dropout_h)[0]
         src = src + self.dropout1(src2)
         src = self.norm1(src)
-        src2 = self.linear2(self.dropout(F.gelu(self.linear1(src))))
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
         src = src + self.dropout2(src2)
         src = self.norm2(src)
         return src
@@ -96,6 +98,8 @@ class TransformerDecoderLayer(Module):
         self.dropout2 = Dropout(dropout)
         self.dropout3 = Dropout(dropout)
 
+        self.activation = F.gelu
+
     def forward(self, tgt: Tensor, memory: Tensor, tgt_mask: Optional[Tensor] = None, memory_mask: Optional[Tensor] = None,
                 tgt_key_padding_mask: Optional[Tensor] = None, memory_key_padding_mask: Optional[Tensor] = None,
                 dropout_h: float = 0.) -> Tensor:
@@ -109,7 +113,7 @@ class TransformerDecoderLayer(Module):
                                    dropout_h=dropout_h)[0]
         tgt = tgt + self.dropout2(tgt2)
         tgt = self.norm2(tgt)
-        tgt2 = self.linear2(self.dropout(F.gelu(self.linear1(tgt))))
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout3(tgt2)
         tgt = self.norm3(tgt)
         return tgt

preprocess/training_data/04_calculate_weights.py
@@ -14,20 +14,21 @@ if __name__ == '__main__':
     train_df = pd.read_csv(processed_labels_path)
     train_df.fillna('', inplace=True)
 
-    complexity_df = np.sqrt(train_df.iloc[:, 13].apply(lambda x: x.count('-') + x.count('-') * x.count('(') + x.count('(')))
-    complexity_df = complexity_df / complexity_df.sum()
+    print('Complexity')
+    complexity_df = train_df.iloc[:, 13].apply(lambda x: x.count('-') + x.count('-') * x.count('(') + x.count('('))
 
+    print('Atom counts')
     atom_counts_df = train_df.iloc[:, 1:13].sum(axis=1)
-    atom_counts_df = atom_counts_df / atom_counts_df.sum()
 
+    print('Atom rarity')
     atom_present = (train_df.iloc[:, 1:13] > 0)
-    atom_rarity_df = (atom_present / np.power(atom_present.sum(axis=0), 0.5)).sum(axis=1)
-    atom_rarity_df = atom_rarity_df / atom_rarity_df.sum()
+    atom_rarity_df = (atom_present/atom_present.sum(axis=0)).sum(axis=1)
 
+    print('Layer rarity')
     layer_present = (train_df.iloc[:, 13:20] != '')  # ih,ib,it,im,is not considered
-    layer_rarity_df = (layer_present / np.power(layer_present.sum(axis=0), 0.5)).sum(axis=1)
-    layer_rarity_df = layer_rarity_df / layer_rarity_df.sum()
+    layer_rarity_df = (layer_present/layer_present.sum(axis=0)).sum(axis=1)
 
+    print(f'Saving to {label_weights_path}')
     weights_df = pd.concat([train_df['image_id'], complexity_df, atom_counts_df, atom_rarity_df, layer_rarity_df], axis=1)
     weights_df.columns = ['image_id', 'complexity', 'atom_count', 'atom_rarity', 'layer_rarity']
-    weights_df.to_csv(label_weights_path)
+    weights_df.to_csv(label_weights_path, index=False)

train.py
@@ -36,23 +36,24 @@ if __name__ == '__main__':
     img_size = 384
     bpe_num = 4096
     max_len = 256
-    lr = 3e-4
-    bs = 128
+    lr = 1e-4
+    bs = 96
     BS = None
     epochs_num = 24
     # model
     N, n = 32, 128
-    enc_d_model, enc_nhead, enc_dim_feedforward, enc_num_layers = 768, 24, 4*512, 6
-    dec_d_model, dec_nhead, dec_dim_feedforward, dec_num_layers = 768, 24, 4*512, 6
+    enc_d_model, enc_nhead, enc_dim_feedforward, enc_num_layers =  512, 8, 2048, 6
+    dec_d_model, dec_nhead, dec_dim_feedforward, dec_num_layers =  512, 8, 2048, 6
     #
     div_factor = 1e3
     pct_start = 1 / epochs_num
     final_div_factor = 1.
     # clip grad
     max_norm = 1.0
+    weight_decay = 0.
     #
-    dropout_p = 0.1
-    dropout_h_base = 0.1
+    dropout_p = 0.072
+    dropout_h_base = 0.063
     dropout_dec_emb = 0.1
     #
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
@@ -63,9 +64,14 @@ if __name__ == '__main__':
     imgs_path = ds_path/'images/resized'/str(img_size)/'train'
     imgs_path = imgs_path if imgs_path.exists() else ds_path / 'images/train'
     df = pd.read_csv(ds_path/'train_labels.csv')
-    is_valid = np.array([i%50==0 for i in range(len(df))])
+    is_valid = np.array([i%50==0 for i in range(len(df))])  # Split
     train_df = df.iloc[~is_valid]
     valid_df = df.iloc[is_valid]
+    weights_df = pd.read_csv(ds_path / 'train_labels_weights.csv')[~is_valid]
+    weights_df.values[:,3] = np.power(weights_df.values[:,3], 0.50)  # Atom rarity
+    weights_df.values[:,4] = np.power(weights_df.values[:,4], 0.25)  # Layer rarity
+    weights_df.values[:,1:] = weights_df.values[:,1:]/weights_df.values[:,1:].sum(axis=0)
+    weights = (weights_df.values[:,1:] * np.array([1.4,1.,0.7,0.5])).sum(axis=1).astype(np.float32)
 
     sp.SentencePieceProcessor()
     subwords_path = ds_path/'subwords'/f'bpe_{bpe_num}.model'
@@ -78,7 +84,6 @@ if __name__ == '__main__':
 
     trn_ds = DS(imgs_path, img_size, train_df, swp, max_len, train=True)
     tqdm.pandas()
-    weights = train_df.progress_apply(lambda x: len(x[1])**1, axis=1).to_list()
     trn_sampler = torch.utils.data.WeightedRandomSampler(weights, len(train_df), replacement=True)
     trn_dl = DataLoader(trn_ds, batch_size=bs, sampler=trn_sampler,
                         drop_last=True,
@@ -99,7 +104,7 @@ if __name__ == '__main__':
     # Train params
     total_steps = epochs_num*len(trn_dl)
     loss_fn = nn.CrossEntropyLoss(reduction='none')
-    optimizer = optim.Adam(model.parameters())
+    optimizer = optim.Adam(model.parameters(),weight_decay=weight_decay)
     lr_sched = lr_scheduler.OneCycleLR(optimizer,lr,total_steps,
                                        div_factor=div_factor,pct_start=pct_start,final_div_factor=final_div_factor)
     scaler = torch.cuda.amp.GradScaler()